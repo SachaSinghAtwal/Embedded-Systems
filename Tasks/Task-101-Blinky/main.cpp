39f4a757b666b7e80c0fc7f9b2388327edd47938
Sacha Singh Atwal <sacha.atwal@students.plymouth.ac.uk>
2023-10-04 17:31:32 +0100

    Introduction Lesson
    Simple changes to check software and hardware

--- Tasks/Task-101-Blinky/main.cpp
+++ Tasks/Task-101-Blinky/main.cpp
@@ -1,7 +1,7 @@
 #include "mbed.h"
 
 // Create a DigitalOut “object” called greenLED. Pass constant LED1 as a “parameter”
-DigitalOut greenLED(LED1);
+DigitalOut greenLED(LED2);
 
 //These are "commented out" and so are not part of the program. You can uncomment them by removing the // characters
 //Your task is to make a sequence alternating between Green+Red and just Blue 
@@ -20,12 +20,12 @@ int main()
         greenLED = 1; 
 
         // Wait 0.2 second (1 million microseconds)
-        wait_us(100000); 
+        wait_us(1000000); 
 
         // Turn LED OFF
         greenLED = 0;
 
         // Wait 0.2 second
-        wait_us(100000); 
+        wait_us(1000000); 
     }
 }
